c21224c1e7abb457881b4e97863c4b0cc2fdce7e
Ulric Ye <yye90012@gmail.com>
2017-04-19 20:08:54 -0700

    Created PhotoMapView

---
+++ Photo Map/PhotoMapViewController.swift
@@ -0,0 +1,120 @@
+//
+//  PhotoMapViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MKMapViewDelegate, LocationsViewControllerDelegate {
+    @IBOutlet weak var mapView: MKMapView!{
+        didSet{
+            self.mapView.delegate  = self
+        }
+    }
+    
+    var pickedImage: UIImage!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // One degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        // San Francisco Lat, Long = latitude: 37.783333, longitude: -122.416667
+        let mapCenter = CLLocationCoordinate2D(latitude: 37.783333, longitude: -122.416667)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: mapCenter, span: mapSpan)
+        // Set animated property to true to animate the transition to the region
+        mapView.setRegion(region, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        pickedImage = originalImage
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true) { 
+            self.performSegue(withIdentifier: "tagSegue", sender: nil)
+        }
+    }
+    
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
+        addPin(latitude: latitude, longitude: longitude)
+    }
+
+    func addPin(latitude: NSNumber, longitude: NSNumber) {
+        let annotation = MKPointAnnotation()
+        let locationCoordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude))
+        annotation.coordinate = locationCoordinate
+        annotation.title = "\(latitude)"
+        mapView.addAnnotation(annotation)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseID = "myAnnotationView"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
+        if (annotationView == nil) {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            
+        }
+        
+        let leftView = UIImageView(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+        leftView.image = pickedImage
+        print("hello")
+        annotationView?.leftCalloutAccessoryView = leftView
+        annotationView?.canShowCallout = true
+
+        
+        // Add the image you stored from the image picker
+        
+        return annotationView
+    }
+    
+    @IBAction func tapCamera(_ sender: Any) {
+        /*
+         let vc = UIImagePickerController()
+         vc.delegate = self
+         vc.allowsEditing = true
+         vc.sourceType = UIImagePickerControllerSourceType.camera
+         */
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(vc, animated: true, completion: nil)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+    }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let locationsViewController = segue.destination as! LocationsViewController
+        locationsViewController.delegate = self
+    }
+ 
+
+}
+
